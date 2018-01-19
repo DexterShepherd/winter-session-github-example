42aacf8185c3c7924a117eec192be2d5d489e297
Dexter Shepherd <dexterjshepherd@gmail.com>
2018-01-18 18:02:59 -0800

    less shapes

--- background-sketch.js
+++ background-sketch.js
@@ -13,8 +13,8 @@ function draw() {
   yR = map(mouseY, 0, height, 50, height * 2)
 
 
-  for(let i = 0; i < 50; i += 1) {
-    for(let j = 0; j < 50; j += 1) {
+  for(let i = 0; i < 20; i += 1) {
+    for(let j = 0; j < 10; j += 1) {
       xPos = sin(i + frameCount * 0.005) * sin(j + frameCount * 0.001)  * xR
       yPos = cos(i + frameCount * 0.005) * cos(j + frameCount * 0.001) * yR
       rect(width / 2 + xPos, height / 2 + yPos, 3, 3)
